4eb16dee062bd0121864f02fa12529cc0a587d05
thinhplust <thinhplust@gmail.com>
2022-09-06 13:50:58 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -20,7 +20,7 @@ module.exports = {
   title: "3S-TEAM",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: 'https://lungmat.sharepoint.com/sites/THINHND269',
+  baseDirectory: '/',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
